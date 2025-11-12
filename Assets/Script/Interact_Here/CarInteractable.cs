e2070d37e943bf71f65249dea2c5615aa9591b52
Ratitiy <eart_7028@hotmail.com>
2025-11-13 05:35:41 +0700

    Update CarInteractable.cs

--- Assets/Script/Interact_Here/CarInteractable.cs
+++ Assets/Script/Interact_Here/CarInteractable.cs
@@ -2,12 +2,10 @@
 
 public class CarInteractable : MonoBehaviour, IInteractable
 {
-   
-
     public string prompt = "กด E เพื่อขึ้นรถ";
-    public Transform seat;           
-    public Transform exitPoint;      
-    public Transform cameraTarget;   
+    public Transform seat;
+    public Transform exitPoint;
+    public Transform cameraTarget;
     public SimpleCarController controller;
 
     bool occupied;
@@ -16,15 +14,38 @@ public class CarInteractable : MonoBehaviour, IInteractable
 
     public void Interact(GameObject interactor)
     {
-        if (occupied) return;
+        Debug.Log($"[CarInteractable] Interact called on {name}");
+        if (occupied)
+        {
+            Debug.Log("[CarInteractable] รถถูกใช้งานอยู่แล้ว");
+            return;
+        }
+
         var pvc = interactor.GetComponent<PlayerVehicleController>();
-        if (!pvc) return;
+        if (!pvc)
+        {
+            Debug.LogWarning("[CarInteractable] ไม่พบ PlayerVehicleController บน interactor");
+            return;
+        }
+
         occupied = true;
+        Debug.Log("[CarInteractable] เรียก EnterVehicle()");
         pvc.EnterVehicle(this);
     }
 
-    public void OnFocus() { }
-    public void OnLoseFocus() { }
-    public void SetOccupied(bool v) => occupied = v;
+    public void OnFocus()
+    {
+        Debug.Log("[CarInteractable] OnFocus()");
+    }
 
+    public void OnLoseFocus()
+    {
+        Debug.Log("[CarInteractable] OnLoseFocus()");
+    }
+
+    public void SetOccupied(bool v)
+    {
+        occupied = v;
+        Debug.Log($"[CarInteractable] SetOccupied({v})");
+    }
 }
