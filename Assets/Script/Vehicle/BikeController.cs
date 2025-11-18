6581fdf27706a429ef914bfb9d0981b416179bfb
Ratitiy <eart_7028@hotmail.com>
2025-11-19 04:33:50 +0700

    add

--- Assets/Script/Vehicle/BikeController.cs
+++ Assets/Script/Vehicle/BikeController.cs
@@ -2,20 +2,14 @@
 
 public class BikeController : MonoBehaviour
 {
-    
     public float moveSpeed = 10f;
     public float boostSpeed = 16f;
     public float turnSpeed = 120f;
-    public float brakeForce = 12f;
+    public float brakeForce = 50f;
 
-   
     public Transform exitPoint;
-    
-
-    
     public CamaraSwitch camaraSwitch;
     public Transform motorcycleCamTarget;
-
     public GameObject Drive;
 
     bool isDriving = false;
@@ -27,9 +21,11 @@ public class BikeController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
 
+        
+        rb.centerOfMass = new Vector3(0f, -0.5f, 0f);
     }
 
-    void Update()
+    void FixedUpdate()
     {
         if (!isDriving) return;
 
@@ -39,13 +35,21 @@ public class BikeController : MonoBehaviour
         bool boosting = Input.GetKey(KeyCode.LeftShift);
         float currentSpeed = boosting ? boostSpeed : moveSpeed;
 
-        rb.MovePosition(rb.position + transform.forward * v * currentSpeed * Time.deltaTime);
+        
+        Vector3 move = transform.forward * v * currentSpeed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + move);
 
-        rb.MoveRotation(rb.rotation * Quaternion.Euler(0, h * turnSpeed * Time.deltaTime, 0));
+       
+        Quaternion turnRot = Quaternion.Euler(0, h * turnSpeed * Time.fixedDeltaTime, 0);
+        rb.MoveRotation(rb.rotation * turnRot);
 
+       
         if (Input.GetKey(KeyCode.Space))
-            transform.position -= transform.forward * brakeForce * Time.deltaTime;
+        {
+            rb.velocity = Vector3.Lerp(rb.velocity, Vector3.zero, brakeForce * Time.fixedDeltaTime);
+        }
 
+        // ลงรถ
         if (Input.GetKeyDown(KeyCode.F))
             ExitMotorcycle();
     }
@@ -53,29 +57,27 @@ public class BikeController : MonoBehaviour
     public void EnterMotorcycle(GameObject player)
     {
         currentPlayer = player;
-
         player.GetComponent<PlayerInteraction>().isRiding = true;
-
 
         player.GetComponent<PlayerMovement>().enabled = false;
         player.GetComponent<CharacterController>().enabled = false;
+
         foreach (var r in player.GetComponentsInChildren<Renderer>())
             r.enabled = false;
 
         Drive.SetActive(true);
         isDriving = true;
 
-        
         if (camaraSwitch != null)
             camaraSwitch.SetTarget(motorcycleCamTarget);
-
-        
     }
 
     void ExitMotorcycle()
     {
         if (currentPlayer == null) return;
+
         Drive.SetActive(false);
+
         currentPlayer.transform.position = exitPoint.position;
         currentPlayer.transform.rotation = exitPoint.rotation;
 
@@ -84,17 +86,14 @@ public class BikeController : MonoBehaviour
         currentPlayer.GetComponent<CharacterController>().enabled = true;
 
         foreach (var r in currentPlayer.GetComponentsInChildren<Renderer>())
-          r.enabled = true;
+            r.enabled = true;
 
-
-        
         isDriving = false;
 
         Vector3 pushDir = (currentPlayer.transform.position - transform.position).normalized;
         currentPlayer.transform.position += pushDir * 0.5f;
+
         if (camaraSwitch != null)
             camaraSwitch.SetTarget(currentPlayer.transform);
-
-
     }
 }
