d6f35460e6928143e6c0ed5f24a12d96f7ca11b6
Ratitiy <eart_7028@hotmail.com>
2025-11-18 18:55:45 +0700

    a

--- Assets/Script/Player/PlayerMovement.cs
+++ Assets/Script/Player/PlayerMovement.cs
@@ -3,7 +3,7 @@
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 1.8f;
-    public float runSpeed = 3.6f;
+    public float runSpeed = 5f;
     public float rotationSpeed = 720f;
     private CharacterController controller;
 
@@ -13,8 +13,14 @@ public class PlayerMovement : MonoBehaviour
     public LayerMask detectLayer;
     public Animator anim;
 
+    float idleTimer = 0f;
+    public float idleDelayMin = 4f;
+    public float idleDelayMax = 7f;
+    float nextIdleTime = 0f;
+
     void Start()
     {
+        nextIdleTime = Random.Range(idleDelayMin, idleDelayMax);
         controller = GetComponent<CharacterController>();
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked; 
@@ -34,7 +40,7 @@ public class PlayerMovement : MonoBehaviour
         float currentSpeed = isRunning ? runSpeed : speed;
         controller.SimpleMove(move.normalized * currentSpeed);
 
-        // ส่งค่าให้ Animator
+      
         anim.SetBool("isWalking", move.magnitude > 0.1f);
 
         anim.SetBool("isRunning", isRunning && move.magnitude > 0.1f);
@@ -57,6 +63,28 @@ public class PlayerMovement : MonoBehaviour
         {
             Cursor.visible = true;
             Cursor.lockState = CursorLockMode.None;
+        }
+        
+        //สุ่มท่า idle
+        if (move.magnitude < 0.1f)
+        {
+            idleTimer += Time.deltaTime;
+
+            if (idleTimer >= nextIdleTime)
+            {
+                
+                int randomIdle = Random.Range(1, 3);
+                anim.SetInteger("idleIndex", randomIdle);
+
+                idleTimer = 0f;
+                nextIdleTime = Random.Range(idleDelayMin, idleDelayMax);
+            }
+        }
+        else
+        {
+            
+            anim.SetInteger("idleIndex", 0);
+            idleTimer = 0f;
         }
     }
 
