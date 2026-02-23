10383edfee81190d2150e9939cddeba2a5b243f1
Ratitiy <108119576+Ratitiy@users.noreply.github.com>
2026-02-24 02:33:11 +0700

    Update PlayerMove.cs

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -1,50 +1,65 @@
 ﻿using UnityEngine;
 
-[RequireComponent(typeof(CharacterController))]
 public class PlayerMove : MonoBehaviour
 {
-    [Header("Movement Settings")]
-    public float speed = 12f;          
-    public float gravity = -9.81f;     
-
-    [Header("Ground Check Settings")]
-    public Transform groundCheck;      
-    public float groundDistance = 0.4f;
-    public LayerMask groundMask;      
+    public float speed = 1.8f;
+    public float runSpeed = 5f;
+    public float rotationSpeed = 720f;
 
     private CharacterController controller;
-    private Vector3 velocity;
-    private bool isGrounded;
+    public Animator anim;
+
+    public bool uiOpened = false;    // UI เปิด?
 
     void Start()
     {
-       
         controller = GetComponent<CharacterController>();
+
     }
 
     void Update()
     {
-        
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-        
-        if (isGrounded && velocity.y < 0)
+        if (uiOpened)
         {
-            velocity.y = -2f;
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+
+            controller.SimpleMove(Vector3.zero);
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isRunning", false);
+            return;
+        }
+        else
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
         }
 
-        
-        float x = Input.GetAxis("Horizontal"); 
-        float z = Input.GetAxis("Vertical");   
 
-       
-        Vector3 move = transform.right * x + transform.forward * z;
 
-        // สั่งให้ตัวละครเดิน
-        controller.Move(move * speed * Time.deltaTime);
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
 
-        // 3. คำนวณแรงโน้มถ่วงดึงตัวละครลงพื้น
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
+        Vector3 move = new Vector3(horizontal, 0f, vertical);
+        move = Camera.main.transform.TransformDirection(move);
+        move.y = 0f;
+
+        bool isRunning = Input.GetKey(KeyCode.LeftShift);
+        float currentSpeed = isRunning ? runSpeed : speed;
+        controller.SimpleMove(move.normalized * currentSpeed);
+
+        anim.SetBool("isWalking", move.magnitude > 0.1f);
+        anim.SetBool("isRunning", isRunning && move.magnitude > 0.1f);
+
+        if (move.magnitude > 0.1f)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(move, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(
+                transform.rotation,
+                toRotation,
+                rotationSpeed * Time.deltaTime
+            );
+        }
     }
 }
