99c923fdfaf0cd8315aad10a223214331c2809e1
Ratitiy <108119576+Ratitiy@users.noreply.github.com>
2026-03-10 02:29:01 +0700

    add

---
+++ Assets/Script/TopDown.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class TopDown : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset = new Vector3(0, 15, -5);
+    public float smoothSpeed = 10f;
+    public float rotationSmooth = 5f; 
+
+    void LateUpdate()
+    {
+        if (target == null) return;
+
+       
+        Vector3 desiredPosition = target.TransformPoint(offset);
+
+        
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+
+        
+        Quaternion targetRotation = Quaternion.LookRotation(target.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSmooth * Time.deltaTime);
+    }
+}
