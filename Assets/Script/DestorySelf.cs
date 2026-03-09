b13b2fb2e5f7b8ce4d7856c37ccbe24b8be78415
Ratitiy <108119576+Ratitiy@users.noreply.github.com>
2026-03-10 02:57:45 +0700

    add

---
+++ Assets/Script/DestorySelf.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class DestorySelf : MonoBehaviour
+{
+    [Header("Life Settings")]
+    public float lifeTime = 5f; 
+
+    void Start()
+    {
+       
+        Destroy(gameObject, lifeTime);
+    }
+}
---
+++ Assets/Script/Spawner.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [Header("Spawner Settings")]
+    public GameObject[] obstaclePrefabs; 
+    public Transform[] spawnPoints;      
+
+    public float spawnInterval = 3f;     
+    public bool isSpawning = true;
+
+    private float timer;
+
+    void Update()
+    {
+        if (!isSpawning) return;
+
+        timer += Time.deltaTime;
+
+        if (timer >= spawnInterval)
+        {
+            SpawnObstacle();
+            timer = 0;
+        }
+    }
+
+    void SpawnObstacle()
+    {
+        if (obstaclePrefabs.Length == 0 || spawnPoints.Length == 0) return;
+
+        
+        int randomPoint = Random.Range(0, spawnPoints.Length);
+        Transform sp = spawnPoints[randomPoint];
+
+       
+        int randomPrefab = Random.Range(0, obstaclePrefabs.Length);
+
+       
+        Instantiate(obstaclePrefabs[randomPrefab], sp.position, sp.rotation);
+    }
+}
