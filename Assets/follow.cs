f53f846b60cfa2b3ea598ce515d913fe58a57b32
Ratitiy <108119576+Ratitiy@users.noreply.github.com>
2026-04-07 07:56:35 +0700

    add

---
+++ Assets/follow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class follow : MonoBehaviour
+{
+    public Transform target; 
+    public Vector3 offset = new Vector3(0, 30f, 0);
+
+    void LateUpdate()
+    {
+        if (target != null)
+        {
+            
+            transform.position = target.position + offset;
+
+            
+            float targetYRotation = target.eulerAngles.y;
+
+            
+            transform.rotation = Quaternion.Euler(0, targetYRotation, 0);
+        }
+    }
+}
