18bd7533076259da8ae980aec260dd01c4fca3f4
Ratitiy <108119576+Ratitiy@users.noreply.github.com>
2026-04-07 13:28:16 +0700

    add

---
+++ Assets/MapM.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class MapM : MonoBehaviour
+{
+    public GameObject largeMapPanel;
+    private bool isMapOpen = false;
+
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            ToggleMap();
+        }
+    }
+
+    void ToggleMap()
+    {
+        isMapOpen = !isMapOpen; 
+        largeMapPanel.SetActive(isMapOpen); 
+
+        if (isMapOpen)
+        {
+            Time.timeScale = 0.2f; 
+        }
+        else
+        {
+            Time.timeScale = 1f; 
+        }
+    }
+}
